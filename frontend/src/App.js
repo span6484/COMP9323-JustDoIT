737ef6e169879e22468cc6915e039f2c645c113f
Yuchao Fang <asdf551100@gmail.com>
2022-07-11 20:27:41 +1000

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Get started with COMP9323
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
