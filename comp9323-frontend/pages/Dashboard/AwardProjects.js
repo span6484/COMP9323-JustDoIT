ecdc98421c9e93a6b174a3cc72075b3a41488ba2
kelurLucky <Lucky@kelur.com>
2022-07-13 22:57:10 +0800

    my projects

--- comp9323-frontend/pages/Dashboard/AwardProjects.js
+++ comp9323-frontend/pages/Dashboard/AwardProjects.js
@@ -1,11 +1,105 @@
 import PageBase from '../basePage'
-import React ,{useRef} from 'react'
-
+import React ,{useRef,useState,useEffect} from 'react'
+import AwardProjectsStyle from "./AwardProjects.less"
 const AwardProjects = ({ USERMESSAGE }) => {
   const ref = useRef();
+  const [awardList,changeAwardList] = useState([]);
+  useEffect(()=>{
+    const _list = [ {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}, {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}, {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}, {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}, {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}, {
+      projectName : "English",
+      course : "aaa",
+      courseAuthority : "adad",
+      currentStudentNumber : 1,
+      maxStudentNumber : 3,
+      startTime : "2010-12-11 13:40:20",
+      closeTime : "2022-12-11 13:40:20",
+      statues : "4"}];
+    changeAwardList(_list);
+  },[])
+  function itemDom(value){
+    return <div
+      style={{
+        width : "calc(100% - 125px)"
+      }}
+      className={"award-project-component-tab-name"}>
+      {value}
+    </div>
+  }
   return (
     <PageBase cRef={ref} USERMESSAGE={USERMESSAGE}>
-      <div>Award</div>
+      <style dangerouslySetInnerHTML={{
+        __html: AwardProjectsStyle
+      }}/>
+      <div className={"award-project-component"}>
+        <div className={"awardList-box"}>
+        {
+          awardList && awardList.map((item,index) => {
+            return <div className={"award-project-component-item"} key={"award-project-component-" + index}>
+               <div className={"award-project-component-tab"}>
+                 <h6>Project name:</h6>
+                 {itemDom(item.projectName)}
+               </div>
+              <div className={"award-project-component-tab"}>
+                <h6>Course:</h6>
+                {itemDom(item.course)}
+              </div>
+              <div className={"award-project-component-tab"}>
+                <h6>Course Authority:</h6>
+                {itemDom(item.courseAuthority)}
+              </div>
+              <div className={"award-project-component-tab"}>
+                <h6>Proposer:</h6>
+                {itemDom(item.proposer)}
+              </div>
+              <div className={"award-project-component-tab"}>
+                <h6>Group members:</h6>
+                {itemDom(item.groupMembers)}
+              </div>
+            </div>
+          })
+        }
+        </div>
+      </div>
     </PageBase>
   )
 }
--- comp9323-frontend/pages/Dashboard/CourseOverview.js
+++ comp9323-frontend/pages/Dashboard/CourseOverview.js
@@ -1,11 +1,71 @@
 import PageBase from '../basePage'
-import React ,{useRef} from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
+import CourseOverviewStyle from "./CourseOverview.less";
+import {Pagination} from "antd"
 const CourseOverview = ({ USERMESSAGE }) => {
   const ref = useRef();
+  const [courseList,changeCourseList] = useState([]);
+  const [page,changePage] = useState({
+    total : 20,
+    number : 1,
+    size : 10
+  });
+  const [loading, changeLoading] = useState(false)
+  useEffect(()=>{
+    const _list = [ {}, {}, {}, {}, {},{}, {}, {}, {}, {},{}, {}];
+    changeCourseList(_list);
+  },[])
+  function itemDom(value){
+    return <div
+      style={{
+        width : "calc(100% - 125px)"
+      }}
+      className={"award-project-component-tab-name"}>
+      {value}
+    </div>
+  }
+  function searchList(initPage) {
+    initPage = initPage || page
+  }
   return (
     <PageBase cRef={ref} USERMESSAGE={USERMESSAGE}>
-      <div>Course</div>
+      <style dangerouslySetInnerHTML={{
+        __html: CourseOverviewStyle
+      }}/>
+      <div className={"course-overview-component"}>
+        <div className={"awardList-box"}>
+          {
+            courseList && courseList.map((item,index) => {
+              return <div className={"award-project-component-item"} key={"award-project-component-" + index}>
+                      <h5>CourseName:</h5>
+                      <h6>Duration</h6>
+                  </div>
+            })
+          }
+        </div>
+        {
+          page.total > 0 && <div className={"list-detail-pagination"}>
+            <Pagination
+              current={page.number}
+              total={page.total}
+              showQuickJumper
+              simple
+              pageSize={page.size}
+              onChange={(pageIndex,pageSize)=>{
+                if(loading){
+                  message.warning("Searching, please wait");
+                  return;
+                }
+                const _page = _.cloneDeep(page);
+                _page.number = pageIndex <= 0 ? 1 : pageIndex;
+                _page.size = pageSize;
+                changePage(_page);
+                searchList(_page);
+              }}
+            />
+          </div>
+        }
+      </div>
     </PageBase>
   )
 }
