d5a92374d8394a267d7f031bb9eedf2bf6e387d2
yx5685 <yaxin.su@student.unsw.edu.au>
2022-07-15 04:39:31 +0800

    create-table-courses-users-cu-and-insert-data

--- comp9323-backend/app/models.py
+++ comp9323-backend/app/models.py
@@ -12,12 +12,62 @@ def init_db(app):
 
 
 class UserModel(db.Model):
+
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(256), nullable=False)
+    uid = db.Column(db.String(256), primary_key=True)
+    id = db.Column(db.String(256), unique=True, nullable=False)
+    role = db.Column(db.Integer, nullable=False)  # 0: CA，1: S，2: P，3: R
+
+    username = db.Column(db.String(120))
+    email = db.Column(db.String(256), unique=True)
+    password = db.Column(db.String(256))
+    detail = db.Column(db.TEXT)  # user detail
+
+    ctime = db.Column(db.DateTime, nullable=False)  # create time
+    utime = db.Column(db.DateTime, nullable=False)  # update time
+    active = db.Column(db.Integer, nullable=False, default=1)  # 0:delete,  1:not delete
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+
+class CourseModel(db.Model):
+
+    __tablename__ = 'courses'
+
+    cid = db.Column(db.String(256), primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    description = db.Column(db.TEXT)  # course description
+    start_time = db.Column(db.DateTime, nullable=False)     # start time
+    close_time = db.Column(db.DateTime, nullable=False)     # close time
+    public = db.Column(db.Integer, nullable=False, default=0)   # 0: private, 1: public
+    ctime = db.Column(db.DateTime, nullable=False)  # create time
+    utime = db.Column(db.DateTime, nullable=False)  # update time
+    active = db.Column(db.Integer, nullable=False, default=1)  # 0:delete, 1:not delete
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class CourseUser(db.Model):
+    # key: cuid
+    # foreign key: cid, uid(users.role = 0, 1)
+    # ctime, utime, active
+
+    __tablename__ = 'course_user'
+
+    cuid = db.Column(db.String(256), primary_key=True)
+    cid = db.Column(db.String(256), nullable=False)  # courses.cid
+    uid = db.Column(db.String(256), nullable=False)  # users.uid
+
+    ctime = db.Column(db.DateTime, nullable=False)  # create time
+    utime = db.Column(db.DateTime, nullable=False)  # update time
+    active = db.Column(db.Integer, nullable=False, default=1)  # 0:delete, 1:not delete
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
