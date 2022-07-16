62da608826a7737b7afda0a445df4f23d48b7899
kelurLucky <Lucky@kelur.com>
2022-07-16 23:33:38 +0800

    tab router

--- comp9323-frontend/pages/project/edit.js
+++ comp9323-frontend/pages/project/edit.js
@@ -1,17 +1,18 @@
 import PageBase from '../basePage';
 import projectStyle from "./project.less";
-
+import moment from 'moment';
 import React, { useRef, onChange, useState } from 'react';
 import { UploadOutlined } from '@ant-design/icons';
-import { Col, Row, Button, Typography, Input, Space, Select, message, Upload, Comment, Avatar } from 'antd';
+import {Col, Row, Button, Typography, Input, Space, Select, message, Upload, Comment, Avatar,DatePicker, Steps} from 'antd';
 const { Dragger } = Upload;
 import { SP } from 'next/dist/next-server/lib/utils';
 const { Title, Paragraph, Text, Link } = Typography;
-
+const { Step } = Steps;
+const dateFormat = 'YYYY/MM/DD';
 const TextIndex = ({ USERMESSAGE }) => {
     const ref = useRef();
     const { Option } = Select;
-
+    var role = "CA"
 
     const fileList = [
         {
@@ -26,6 +27,45 @@ const TextIndex = ({ USERMESSAGE }) => {
             status: 'error',
         },
     ];
+
+    function ProgressBars(props) {
+        const userRole = props.userRole;
+        if (userRole == "CA" || userRole == "R") {
+            return (
+                <>
+                    <Steps current={1}>
+                        <Step title="Pending" description="Project being reviewed" />
+                        <Step title="Approved" description="Approved by course authority" />
+                        <Step title="Open to join" description="Open to student to join" />
+                        <Step title="In Progress" description="Project in progress" />
+                        <Step title="Ended" description="Student works are submitted" />
+                    </Steps>
+                </>
+            );
+        } else if (userRole == "P") {
+            return (
+                <>
+                    <Steps current={1}>
+                        <Step title="Pending" description="Project being reviewed" />
+                        <Step title="Not Approved" description="Not approved by course authority" />
+                    </Steps>
+                </>
+            );
+        }
+        return (
+            <>
+                <Steps current={0}>
+                    <Step title="Open to join" description="Open to student to join" />
+                    <Step title="In Progress" description="Project in progress" />
+                    <Step title="Ended" description="Student works are submitted" />
+                </Steps>
+            </>
+        );
+    }
+    const disabledDate = (current) => {
+        // Can not select days before today and today
+        return current && current < moment().endOf('day');
+    };
     return (
         <PageBase cRef={ref} USERMESSAGE={USERMESSAGE}>
             <style dangerouslySetInnerHTML={{
@@ -40,24 +80,48 @@ const TextIndex = ({ USERMESSAGE }) => {
                         <Space direction="vertical" size="middle" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start' }}>
                             <Title>Edit Project</Title>
                             <br />
-                            <Title level={2}>Example project name</Title>
-                            <br />
-                            <Paragraph>
-                                More than 1.2 billion invoices are exchanged in Australia every year, with around 90 percent of invoice processing still partly or fully manual. Over the past 2 years, the Government has invested nearly $20M to facilitate e-invoicing adoption across Australia. In New South Wales state government, agencies will have to use e-invoicing for goods and services valued at up to AUD 1 million from 2022. It is expected that this will be extended to all transactions in the longer term. The use of e-invoicing requires each company participating in an e-invoice exchange to have a specialised software infrastructure to satisfy existing regulations. Most provided solutions are in the form of a complete package that offers several functionalities for participating in the e-invoicing exchange. However, such solutions may not be suitable in all contexts and are often expensive or tied to the use of other products. For example, Xero offers e-invoicing facilities as part of their cloud solution, but a company would need to migrate all their accounting system to Xero first before they can use them. Therefore, there is a need to offer custom-made solutions for niche areas that will address the requirements of small players like SMEs.
-                            </Paragraph>
-                            <br />
+                            {/*<Title level={2}>Example project name</Title>*/}
+                            {/*<br />*/}
+                            {/*<Paragraph>*/}
+                            {/*    More than 1.2 billion invoices are exchanged in Australia every year, with around 90 percent of invoice processing still partly or fully manual. Over the past 2 years, the Government has invested nearly $20M to facilitate e-invoicing adoption across Australia. In New South Wales state government, agencies will have to use e-invoicing for goods and services valued at up to AUD 1 million from 2022. It is expected that this will be extended to all transactions in the longer term. The use of e-invoicing requires each company participating in an e-invoice exchange to have a specialised software infrastructure to satisfy existing regulations. Most provided solutions are in the form of a complete package that offers several functionalities for participating in the e-invoicing exchange. However, such solutions may not be suitable in all contexts and are often expensive or tied to the use of other products. For example, Xero offers e-invoicing facilities as part of their cloud solution, but a company would need to migrate all their accounting system to Xero first before they can use them. Therefore, there is a need to offer custom-made solutions for niche areas that will address the requirements of small players like SMEs.*/}
+                            {/*</Paragraph>*/}
+                            {/*<br />*/}
 
                             <Title level={4}>Change project name</Title>
-                            <Input placeholder="Enter new project name here" />
+                            <Input
+                                value={"Example project name"}
+                                placeholder="Enter new project name here" />
                             <br />
                             <Title level={4}>Change project description</Title>
-                            <Input placeholder="Enter new project description here" />
+                            <Input.TextArea
+                                maxLength={1200}
+                                autoSize={{ minRows: 4, maxRows: 8 }}
+                                value={"More than 1.2 billion invoices are exchanged in Australia every year, with around 90 percent of invoice processing still partly or fully manual. Over the past 2 years, the Government has invested nearly $20M to facilitate e-invoicing adoption across Australia. In New South Wales state government, agencies will have to use e-invoicing for goods and services valued at up to AUD 1 million from 2022. It is expected that this will be extended to all transactions in the longer term. The use of e-invoicing requires each company participating in an e-invoice exchange to have a specialised software infrastructure to satisfy existing regulations. Most provided solutions are in the form of a complete package that offers several functionalities for participating in the e-invoicing exchange. However, such solutions may not be suitable in all contexts and are often expensive or tied to the use of other products. For example, Xero offers e-invoicing facilities as part of their cloud solution, but a company would need to migrate all their accounting system to Xero first before they can use them. Therefore, there is a need to offer custom-made solutions for niche areas that will address the requirements of small players like SMEs."}
+                                placeholder="Enter new project description here" />
 
                         </Space>
-
                         <br />
+                        <Row>
+                            <Col span={20}>
+                                <Title level={4}>Start Time - End Time</Title>
+                                <Title level={5}>
+                                    <DatePicker.RangePicker
+                                        disabledDate={disabledDate}
+                                        defaultValue={[moment('2022/07/26', dateFormat), moment('2022/07/28', dateFormat)]}
+                                        format={dateFormat}
+                                    />
+                                </Title>
+                            </Col>
+                        </Row>
+                        <br />
+                        <Title level={3}>Project current progress</Title>
+                        <br />
+                        <ProgressBars userRole={role} />
+                        <br/>
                         <Title level={3}>Change project progress</Title>
-                        <Select defaultValue="Open to join" style={{ width: 400 }}>
+                        <Button></Button>
+                        <Select defaultValue={"2"}
+                                disabled={true} style={{ width: 400 }}>
                             <Option value="0">Pending</Option>
                             <Option value="1">Approved</Option>
                             <Option value="2">Open to join</Option>
