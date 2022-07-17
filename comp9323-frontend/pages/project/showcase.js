e4f8aecc23d50134c12ebf6ff118772764666799
Yuchao <z5155149@ad.unsw.edu.au>
2022-07-17 16:19:11 +1000

    update data in pages

--- comp9323-frontend/pages/project/showcase.js
+++ comp9323-frontend/pages/project/showcase.js
@@ -42,8 +42,8 @@ const TextIndex = ({ USERMESSAGE }) => {
                         <Row>
                             <Col span={24}>
                                 <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-                                    <Title>Example project name</Title>
-                                    <Title level={2}>A project for Example course name</Title>
+                                    <Title>Natural Language Processing with Disaster Tweets</Title>
+                                    <Title level={2}>A project for Machine Learning and Data Mining</Title>
                                     <Row>
                                         <Col span={12}>
                                             <Title level={4}>Start Time</Title>
@@ -55,23 +55,7 @@ const TextIndex = ({ USERMESSAGE }) => {
                                         </Col>
                                     </Row>
                                     <Paragraph>
-                                        Example project description
-                                        <br />
-                                        More than 1.2 billion invoices are exchanged in Australia every year, with around 90 percent
-                                        of invoice processing still partly or fully manual. Over the past 2 years, the Government has
-                                        invested nearly $20M to facilitate e-invoicing adoption across Australia. In New South Wales
-                                        state government, agencies will have to use e-invoicing for goods and services valued at up to
-                                        AUD 1 million from 2022. It is expected that this will be extended to all transactions in the
-                                        longer term.
-                                        The use of e-invoicing requires each company participating in an e-invoice exchange to have
-                                        a specialised software infrastructure to satisfy existing regulations. Most provided solutions
-                                        are in the form of a complete package that offers several functionalities for participating in
-                                        the e-invoicing exchange. However, such solutions may not be suitable in all contexts and are
-                                        often expensive or tied to the use of other products. For example, Xero offers e-invoicing
-                                        facilities as part of their cloud solution, but a company would need to migrate all their
-                                        accounting system to Xero first before they can use them. Therefore, there is a need to offer
-                                        custom-made solutions for niche areas that will address the requirements of small players like
-                                        SMEs.
+                                        Twitter has become an important communication channel in times of emergency. The ubiquitousness of smartphones enables people to announce an emergency they’re observing in real-time. Because of this, more agencies are interested in programatically monitoring Twitter (i.e. disaster relief organizations and news agencies).
                                     </Paragraph>
 
                                 </Space>
@@ -134,7 +118,7 @@ const TextIndex = ({ USERMESSAGE }) => {
                         <br />
                         <Row>
                             <Col span={24} >
-                                <Title level={2}>Example work to showcase</Title>
+                                <Title level={2}>Awarded work to showcase</Title>
                                 <br />
 
                                 <Title level={3}>Author of project</Title>
