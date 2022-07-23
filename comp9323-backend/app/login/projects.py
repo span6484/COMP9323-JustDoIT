dd5d59c5307d6b7624f152bc55a425288503edfc
span6484 <stephenpsq@gmail.com>
2022-07-23 08:35:47 +0800

    add change_project_status part

--- comp9323-backend/app/login/projects.py
+++ comp9323-backend/app/login/projects.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request, g
 from sqlalchemy import exists
-
+from datetime import datetime as dt
 from app.login.utils import *
 from app.models import *
 
@@ -61,6 +61,7 @@ def view_project():
 
     return jsonify({'code': 200, 'result': result})
 
+# change project status: pending, approved/not, add to join"
 def change_project_status():
     data = request.get_json(force=True)
     proj_id = data["proj_id"]
@@ -88,8 +89,44 @@ def change_project_status():
         if proj.aid != uid:
             return jsonify({'code': 400, 'msg': 'This CA has not access to modify'})
     if role == 0 or role == 3:
-        proj.status = status
-        db.session.commit()
-        return jsonify({'code': 200, 'msg': 'modify status successfully'})
+        if status < 0 or status > 3:
+            return jsonify({'code': 400, 'msg': 'You only could modify status from 0 to 3, 0 : pending, 1: approved, 2: reject 3: join'})
+        else:
+            proj.status = status
+            db.session.commit()
+            return jsonify({'code': 200, 'msg': 'modify status successfully'})
     else:
         return jsonify({'code': 400, 'msg': 'You have no access to modify'})
+
+# change_project status according to start_time
+def change_project_status2():
+    data = request.get_json(force=True)
+    proj_id = data["proj_id"]
+    proj = ProjectModel.query.filter(ProjectModel.proj_id == proj_id).first()
+    if not proj:
+        return jsonify({'code': 400, 'msg': 'not related project'})
+    course_id = proj.cid
+    course = CourseModel.query.filter(CourseModel.cid == course_id and CourseModel.active == 1).first()
+    if not course:
+        return jsonify({'code': 400, 'msg': 'not related course'})
+
+    if proj.status < 3:
+        return jsonify({'code': 400, 'msg': 'this project is still not open to join'})
+    else:
+        now = dt.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        start_time = proj.start_time
+        close_time = proj.close_time
+        print("current time: ", current_time)
+        print("start time: ", start_time)
+        print("close time: ",close_time)
+        if now > close_time:
+            proj.status = 5     # close
+            db.session.commit()
+            return jsonify({'code': 200, 'msg': 'modify status to 5 (Close) successfully'})
+        if now > start_time:
+            proj.status = 4     # in progress
+            db.session.commit()
+            return jsonify({'code': 200, 'msg': 'modify status to 4 (In Progress) successfully'})
+        else:
+            return jsonify({'code': 200, 'msg': 'Wait the start time'})
