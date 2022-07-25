7ad406a38051898bf44d272e4e0c2188dd1e41f0
yx5685 <yaxin.su@student.unsw.edu.au>
2022-07-25 18:13:48 +0800

    get-courses

--- comp9323-backend/app/login/__init__.py
+++ comp9323-backend/app/login/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
-from . import views, projects
+from . import views, projects, course
 
 
 def init_route(app: Flask):
+    # view
     app.add_url_rule('/login', view_func=views.login, methods=['POST'])
     app.add_url_rule('/register', view_func=views.register, methods=['POST'])
     app.add_url_rule('/check_login', view_func=views.check_login)
+    # project
     app.add_url_rule('/view_project', view_func=projects.view_project, methods=['POST'])
     app.add_url_rule('/change_project_status', view_func=projects.change_project_status, methods=['POST'])
     app.add_url_rule('/change_project_status2', view_func=projects.change_project_status2, methods=['POST'])
@@ -13,4 +15,6 @@ def init_route(app: Flask):
     app.add_url_rule('/add_comment', view_func=projects.add_comment, methods=['POST'])
     app.add_url_rule('/reply_comment', view_func=projects.reply_comment, methods=['POST'])
     app.add_url_rule('/delete_comment', view_func=projects.delete_comment, methods=['POST'])
+    # course
+    app.add_url_rule('/get_courses', view_func=course.get_courses, methods=['POST'])
 
--- comp9323-backend/app/login/course.py
+++ comp9323-backend/app/login/course.py
@@ -7,14 +7,18 @@ from app.models import *
 
 def get_courses():
     data = request.get_json(force=True)
-    uid = data[0]
+    print(data)
+    uid = data["uid"]
     user = UserModel.query.filter(UserModel.uid == uid, UserModel.active == 1).first()
     if not user:
         return jsonify({'code': 400, 'msg': 'No such user in database.'})
 
     try:
         # proposer 不用查这个表
-        course_list = CourseUserModel.query.filter(CourseUserModel.uid == uid, CourseUserModel.active == 1).all()
+        cu_list = CourseUserModel.query.filter(CourseUserModel.uid == uid, CourseUserModel.active == 1).all()
+        course_list = []
+        for c in cu_list:
+            course_list.append(CourseModel.query.filter(CourseModel.cid == c.cid, CourseUserModel.active == 1).first())
         if user.role == 0:
             print("CA")
         elif user.role == 1:
@@ -26,11 +30,11 @@ def get_courses():
             print("Reviewer")
             review_list = CourseModel.query.filter(CourseModel.public == 1, CourseModel.active == 1).all()
             course_list.extend(review_list)
-
+        print(course_list)
         result = {"count": len(course_list)}
         result_list = []
         for c in course_list:
-            c_dict = {"cid": c.cid, "name": c.name, "start_time": c.start_time, "close_time": c.closetime}
+            c_dict = {"cid": c.cid, "name": c.name, "start_time": c.start_time, "close_time": c.close_time}
             result_list.append(c_dict)
         result["c_list"] = result_list
         return jsonify({'code': 200, 'result': result})
