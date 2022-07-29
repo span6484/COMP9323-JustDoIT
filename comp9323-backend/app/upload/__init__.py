2763a025b666ba8abbe44263f5988f44bd7c8345
span6484 <stephenpsq@gmail.com>
2022-07-29 09:35:28 +0800

    add file upload function

--- comp9323-backend/app/upload/__init__.py
+++ comp9323-backend/app/upload/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask import Flask, flash, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import os
-
+from app.models import *
+from app.login.utils import *
 def init_route(app: Flask):
     # upload File
     UPLOAD_FOLDER = './upload_files/'
@@ -22,6 +23,10 @@ def init_route(app: Flask):
                 flash('No file part')
                 return redirect(request.url)
             file = request.files['file']
+            result = request.form
+            proj_id = result["proj_id"]
+            uid = result["uid"]
+            # print(uid)
             # If the user does not select a file, the browser submits an
             # empty file without a filename.
             if file.filename == '':
@@ -29,11 +34,19 @@ def init_route(app: Flask):
                 flash('No selected file')
                 return redirect(request.url)
             if file and allowed_file(file.filename):
-                print("if-3")
+                # print("if-3")
                 filename = secure_filename(file.filename)
-                print(filename)
-                print(os.path.join(basedir,app.config['UPLOAD_FOLDER'], filename))
+                # print(filename)
+                path = os.path.join(basedir,app.config['UPLOAD_FOLDER'], filename)
                 file.save(os.path.join(basedir, app.config['UPLOAD_FOLDER'], filename))
+                # print("proj_id: ", proj_id)
+                # print("uid: ", uid)
+                f_num = FileModel.query.count()
+                fid = generate_id("file", f_num)
+                date_time = get_time()[0]
+                add_file = FileModel(fid = fid, proj_id = proj_id, uid = uid, file_name = filename, file_url = path, type = file.content_type, ctime=date_time, utime=date_time, active=1)
+                db.session.add(add_file)
+                db.session.commit()
                 # return redirect(url_for('download_file', upload_file=filename))
                 return jsonify({'code': 200, 'msg': 'file success'})
         else:
